ddca868e61591108dd4265a93d46771f8589f131
Алексей Захаров <zakharov.nnov@gmail.com>
2021-10-31 19:07:47 +0300

    Добавьте файлы проекта.

---
+++ lesson21/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lesson21
+{
+    //Имеется пустой участок земли (двумерный массив) и план сада, который необходимо реализовать.
+    //Эту задачу выполняют два садовника, которые не хотят встречаться друг с другом.
+    //Первый садовник начинает работу с верхнего левого угла сада и перемещается слева направо, сделав ряд, он спускается вниз.
+    //Второй садовник начинает работу с нижнего правого угла сада и перемещается снизу вверх, сделав ряд, он перемещается влево.
+    //Если садовник видит, что участок сада уже выполнен другим садовником, он идет дальше. Садовники должны работать параллельно.
+    //Создать многопоточное приложение, моделирующее работу садовников.
+    public class Garden
+    {
+        const int n = 10;
+        const int m = 7;
+        int[,] location = new int[n, m];
+
+        public void WorkGardener1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (location[i, j] == 0)
+                    {
+                        location[i, j] = 1;
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+        }
+        public void WorkGardener2()
+        {
+            for (int j = m - 1; j > -1; j--)
+            {
+                for (int i = n - 1; i > -1; i--)
+                {
+                    if (location[i, j] == 0)
+                    {
+                        location[i, j] = 2;
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+        }
+        public void Rezult()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" {0}", location[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Garden garden = new Garden();
+            ThreadStart threadStart = new ThreadStart(garden.WorkGardener1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            garden.WorkGardener2();
+            garden.Rezult();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson21/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lesson21")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lesson21")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c8088524-92d3-4e00-89cd-c58b778b113c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
